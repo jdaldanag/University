5016395c32bab4ff02e428d1af56b8768a67e465
jdaldanag <jdaldanag@gmail.com>
2016-09-01 08:36:47 -0500

    Update Main.java

--- src/university/Main.java
+++ src/university/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Student David = new Student("David","123.456");
+        Graduate Alan = new Graduate("Alan","812.672");
         Professor John = new Professor("433.212");
         Department Ing = new Department("Ingenieria");
         String birthS = "1995.05.22";
@@ -11,15 +12,20 @@ public class Main {
         
         David.setBirthDate(birthS);
         John.setBirthDate(birthP);
+        Alan.setBirthDate(birthS);
         David.setDepartment(Ing);
         John.setDepartment(Ing);
         John.setName("Johnathan");
         John.setAdvisee(David);
         Ing.setChairman(John);
+        Alan.setDegree("ing. Sistemas");
+        Alan.setInstitution("Universidad Nacional");
         
         David.printAll();
         System.out.println("");
         John.printAll();
+        System.out.println("");
+        Alan.printAll();
     }
     
 }
